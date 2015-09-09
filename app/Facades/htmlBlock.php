99b35a7cb077f7d589de9cd1ea56f6d39f57b9af
tabuna <bliz48rus@gmail.com>
2015-09-09 15:25:14 +0300

    Bloki

--- app/Facades/htmlBlock.php
+++ app/Facades/htmlBlock.php
@@ -7,22 +7,6 @@ use Illuminate\Support\Facades\Facade;
 class htmlBlock extends Facade
 {
 
-    static function getLI($NameMenu, $pref = "")
-    {
-        $menu = SiteMenu::where('name', $NameMenu)->first();
-        if (is_null($menu))
-            return false;
-
-
-        $element = $menu->getElement()->get();
-        $html = '';
-        foreach ($element as $li) {
-            $html .= "<li class='$li->class'><a href='$li->link'> $pref $li->label</a></li>";
-        }
-
-        return $html;
-    }
-
     static function getGoodSlider($BlockId)
     {
         $Block = Block::find($BlockId);
