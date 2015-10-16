3ea75ea9bc834e3383c778cf9eb653e78dfb2cff
tabuna <bliz48rus@gmail.com>
2015-10-16 16:41:07 +0300

    Валидация

--- resources/lang/en/alert.php
+++ resources/lang/en/alert.php
@@ -3,9 +3,9 @@
 return [
 
     "You have successfully added a comment" => "You have successfully added a comment",
-    "Thank you for writing, we will respond to you." => "Thank you for writing, we will respond to you.",
-    "You have successfully changed values" => "You have successfully changed values",
-    "You have successfully taken the task" => "You have successfully taken the task",
+    "Thank you for writing, we will respond to you." => "Thank you for contacting us, we will respond to you soon.",
+    "You have successfully changed values" => "You have successfully changed the values",
+    "You have successfully taken the task" => "You have successfully accepted the task",
     "You have successfully changed the status of the task" => "You have successfully changed the status of the task",
     "You have successfully left a review" => "You have successfully left a review",
 ];
