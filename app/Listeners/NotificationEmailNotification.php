b577fd2e1af57e91ba1b1274da306706df3b4a09
tabuna <bliz48rus@gmail.com>
2015-10-19 12:23:24 +0300

    Отправка сообщений редакторам

--- app/Listeners/NotificationEmailNotification.php
+++ app/Listeners/NotificationEmailNotification.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\NewOrder;
 use App\Events\Notification;
+use App\Models\Task;
 use Config;
 use Mail;
 
@@ -31,5 +32,16 @@ class NotificationEmailNotification
             $message->from(Config::get('link.email'));
             $message->to(Config::get('link.email'))->cc(Config::get('link.email'));
         });
+
+        $Task = Task::findOrFail($event->id);
+        $User = $Task->getUser()->select('phone_notification', 'phone')->first();
+        if ($User->email_notification) {
+            Mail::raw('Задача #' . $event->id . ' была взята в работу', function ($message) use ($User) {
+                $message->from(Config::get('link.email'));
+                $message->to($User->email)->cc($User->email);
+            });
+        }
+
+
     }
 }
--- app/Listeners/NotificationSMSNotification.php
+++ app/Listeners/NotificationSMSNotification.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\NewOrder;
 use App\Events\Notification;
+use App\Models\Task;
 use Config;
 use SMS;
 
@@ -28,5 +29,11 @@ class NotificationSMSNotification
     public function handle(Notification $event)
     {
         SMS::send(Config::get('link.phone'), 'Задача #' . $event->id . ' выла взята в работу');
+
+        $Task = Task::findOrFail($event->id);
+        $User = $Task->getUser()->select('phone_notification', 'phone')->first();
+        if ($User->phone_notification) {
+            SMS::send($User->phone, 'Ваш заказ #' . $event->id . ' ожидает рассмотрения');
+        }
     }
 }
