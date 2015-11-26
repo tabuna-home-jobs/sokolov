64a9f33e4c4b1235529994c2dac1aac6623aa279
tabuna <bliz48rus@gmail.com>
2015-11-26 16:35:55 +0300

    Сканирование роботом Googlebot с учетом языка

--- resources/views/_layout/site.blade.php
+++ resources/views/_layout/site.blade.php
@@ -24,6 +24,11 @@
 
     <link rel="stylesheet" href="{{elixir('build/css/app.css')}}" type="text/css"/>
 
+
+    <link rel="alternate" hreflang="en" href="{{url('/language/en')}}"/>
+    <link rel="alternate" hreflang="ru" href="{{url('/language/ru')}}"/>
+
+
     <meta name='yandex-verification' content='63ebe98ad59d0b4c'/>
     <meta name="google-site-verification" content="fQ49WVkHhim6L6m0tdBt2o2g6onG49bX-ih_rmLXJNs"/>
 
