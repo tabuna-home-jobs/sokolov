a535431bed02ab9bf927bb2c822614347c3edfba
hotaru_tomu <angelforevor@mail.ru>
2016-01-28 09:08:48 +0300

    Исправления переводов

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -3,10 +3,10 @@
 return [
     'Services' => 'Our services',
     'Work' => 'How we work',
-    'Advantages' => 'Our Advantages',
+    'Advantages' => 'Our editors',
     'Order' => 'Order now',
     'News' => 'News and Promotions',
-    'News-sub' => 'All news and events',
+    'News-sub' => 'All news and promotions',
     'Reviews' => 'Customer Reviews',
     'Reviews-sub' => 'All reviews',
     'Learn More' => 'Learn more',
@@ -19,19 +19,19 @@ return [
 
 
     "We employ only experienced scientific academic editors who are experts in a particular scientific field. All our editors - experts in English grammar, spelling and punctuation." =>
-        "We only employ experienced scientific editors who are experts in a particular scientific field. All our editors are experts in English grammar, spelling and punctuation.",
+        "We only employ experienced scientific editors who are experts in their scientific field.",
 
     "Our scientific editors have extensive experience in prestigious academic research institutions. Our editors have a significant number of highly cited publications in scientific journals in English." =>
-        "Our scientific editors have extensive experience working in prestigious academic research institutions and have a significant number of highly cited publications in scientific journals in English.",
+        "All of our editors are experts in English grammar, phrasing, spelling and punctuation.",
 
     "Our scientific editors wrote, edited and reviewed scientific publications and grants." =>
-        "Our scientific editors have authored, edited and reviewed scientific publications and grants.",
+        "Our editors have extensive experience working in prestigious academic research institutions.",
 
     "We know that you need to publish your important scientific discoveries in the English-language journal. The language barrier should never be a barrier to the spread of important ideas and discoveries! We are here to help you succeed!" =>
-        "We know what is required for you to publish your important scientific discoveries in an English language journal. The language barrier should never be an obstacle to dissemination of important ideas and discoveries! We are here to help you succeed!",
+        "Our editors have authored, edited and reviewed research publications, grants, and other scientific materials.",
 
     "If your research is not published - they never had. Many manuscripts are accepted for publication because of the poor quality of the English language. Our editors - experts in English. Our editors - scientific experts with many years of research and publications in prestigious scientific journals." =>
-        "Many manuscripts are rejected for publication because of poor quality of English. Our editors are experts in English, hold advanced Ph.D. degrees in sciences from prestigious United States universities, have many years of research experience and have published in peer-reviewed scientific journals",
+        "Our editors will help you succeed!",
 
     "We guarantee that your paper will not be rejected by the scientific journal of the poor quality of the English language. We re-edit your article - free of charge."
     => "We guarantee that your manuscript will not be rejected by your target scientific journal because of poor quality of English. We will re-edit your article - free of charge!",
@@ -41,25 +41,25 @@ return [
     "We" => "We",
 
 
-    "Editing" => "Editing",
+    "Editing" => "Editing samples",
     "Correcting written
                              English scientific manuscripts
                              and elimination of grammar and
                              spelling mistakes" => "Highlight the uniqueness of your work with editing performed by English experts with advanced Ph.D. degrees",
 
 
-    "Translation" => "Translation",
+    "Translation" => "Translation samples",
     "We provide technical
                              translation from Russian into
                              English" => "Competent and prompt technical translations of scientific manuscripts and other documents from Russian to English",
 
-    "Format" => "Formatting",
+    "Format" => "Formatting samples",
 
 
     "Fit manuscripts under strict
                              foreign scientific standards
                              magazines" => "Identification and correction of formatting that does not meet the guidelines of your target journal",
-    "illustrating" => "Illustration",
+    "illustrating" => "Illustration samples",
     "Creation of scientific graphics
                              according to the illustrations
                              provided sketch or
@@ -77,21 +77,21 @@ return [
     "Search" => "Search",
 
 
-    "lastSlider" => "We provide services for professors, doctoral students, graduate students and all people interested in science.",
+    "lastSlider" => "We provide language services for professors, postdoctoral researchers, graduate students and all interested in science people.",
 
-    "RELIABLE" => "RELIABLE",
-    "RESPONSIBLY" => "RESPONSIBLY",
-    "CONVENIENTLY" => "CONVENIENTLY",
-
-
-    "RELIABLE TEXT" => "Your text guaranteed protected from falling into the wrong hands.",
-    "RESPONSIBLY TEXT" => "We approach our work professionally, if your work is not accepted because of poor English - we will do everything necessary amendments FREE!",
-    "CONVENIENTLY TEXT" => "The final decision on this or that changes always for YOU",
+    "RELIABLE" => "SECURITY",
+    "RESPONSIBLY" => "RESPONSIBILITY",
+    "CONVENIENTLY" => "CONVENIENCE",
 
 
-    "Your guarantees" => "Your guarantees",
+    "RELIABLE TEXT" => "All information you provide to us is 100% secure! Every member of our team signs a non-disclosure agreement!",
+    "RESPONSIBLY TEXT" => "We guarantee that a paper edited by us will not be rejected because of poor English quality. If it is, we will re-edit the manuscript for FREE!",
+    "CONVENIENTLY TEXT" => "We track all changes we make to your document. It is easy for you to make the final decision on each correction!",
 
-    "text-logo" => "Editing of scientific publications, technical translation from English into Russian, formatting and illustrate manuscripts scientists for scientists."
+
+    "Your guarantees" => "Our guarantees",
+
+    "text-logo" => "Editing of scientific papers, translation from Russian to English, formatting and illustration of manuscripts by scientists for scientists"
 
 
 ];
